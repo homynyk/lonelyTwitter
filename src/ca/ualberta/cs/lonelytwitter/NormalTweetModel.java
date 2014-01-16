7f6434e3b80c5da65a1c7b2d2115822ff857c8a1
homynyk <homynyk@ualberta.ca>
2014-01-16 15:51:48 -0700

    Added normal subclass

---
+++ src/ca/ualberta/cs/lonelytwitter/NormalTweetModel.java
@@ -0,0 +1,25 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import java.util.Date;
+
+
+public class NormalTweetModel extends LonelyTweetModel
+{
+
+	public NormalTweetModel(String text)
+	{
+		super(text);
+	}
+
+	@Override
+	public Date getTimestamp()
+	{
+
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getStatus() {
+		return "Normal";
+	}
+}
